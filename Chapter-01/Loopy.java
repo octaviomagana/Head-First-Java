8e53bb03b11ff8e72df37f4bf877c10eb3609c73
Octavio Magana <octaviomagana@gmail.com>
2016-07-16 05:57:48 -0500

    Create Loopy.java
    This program is to show how the While loop works in Java

---
+++ Chapter-01/Loopy.java
@@ -0,0 +1,18 @@
+//This program is to show how the While loop works in Java
+
+public class Loopy {
+
+	public static void main (String[] args) {
+	
+	int x=1;
+	System.out.println("Before the Loop");
+
+	while(x<4) {
+	System.out.println("This is during the Loop");
+	System.out.println("Value is: " + x);
+	x++;
+	}
+	
+	System.out.println("This is after the loop");
+	}
+}
