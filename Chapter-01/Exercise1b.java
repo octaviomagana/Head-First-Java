8c0e637d5c4ade30810cd73b53011f52e05c87ed
Octavio Magana <octaviomagana@gmail.com>
2016-07-16 07:15:03 -0500

    Create Exercise1b.java
    Each of the Java files (exercises) represents a complete source file. 
    	Your job is to play compiler and determine whether each of these files will compile. 
    	If they won’t compile, how would you fix them?

---
+++ Chapter-01/Exercise1b.java
@@ -0,0 +1,26 @@
+	/*Each of the Java files (exercises) represents a complete source file. 
+	Your job is to play compiler and determine whether each of these files will compile. 
+	If they won’t compile, how would you fix them?*/
+
+
+	class Exercise1b {  
+
+		public static void main(String [] args) {    
+
+		int x = 1;    
+
+		while ( x < 10 ) {      
+
+			if ( x > 3) {        
+
+			System.out.println("big x");      
+
+			} 
+
+		x++; //this is the line I introduced to increase the value of X
+		} //End of While  
+
+		} //End of Method
+
+
+	} //End of Class
