ac38df710b2cee4d3bbf63d1a519c637b8674ef2
Octavio Magana <octaviomagana@gmail.com>
2016-07-16 05:58:44 -0500

    Create MyFirstApp.java
    First program just print something in Java

---
+++ Chapter-01/MyFirstApp.java
@@ -0,0 +1,8 @@
+//First program just print something in Java
+public class MyFirstApp {
+
+	public static void main (String[] args) {
+	System.out.println("I Rule!");
+	System.out.println("The World");
+	}
+}
