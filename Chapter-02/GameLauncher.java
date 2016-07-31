a3859cd42166122f137a57a25dc7117b908f0d87
Octavio Magana <octaviomagana@gmail.com>
2016-07-31 12:22:13 -0500

    Create GameLauncher.java
    The guessing game involves a ‘game’ object and three ‘player’ objects. The game generates a random number between 0 and 9, and the three player objects try to guess it.

---
+++ Chapter-02/GameLauncher.java
@@ -0,0 +1,9 @@
+public class GameLauncher {
+
+	public static void main (String[] args) {
+	
+	GuessGame game = new GuessGame();
+	game.startGame();
+	
+	} //End of main method which invokes the Guess Game
+} //end of class GameLauncher
